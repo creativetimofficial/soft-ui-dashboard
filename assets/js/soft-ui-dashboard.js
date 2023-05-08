365d2b0a8b4886010577621e1911a9056355f60f
Stefan <stefan@creative-tim.com>
2023-05-08 12:01:27 +0300

    refactor: add Nepcha script

--- assets/js/soft-ui-dashboard.js
+++ assets/js/soft-ui-dashboard.js
@@ -398,24 +398,23 @@ function toggleSidenav() {
 
 // Resize navbar color depends on configurator active type of sidenav
 
-if (document.querySelector('[data-class]')) {
-  let referenceButtons = document.querySelector('[data-class]');
+let referenceButtons = document.querySelector('[data-class]');
 
-  window.addEventListener("resize", navbarColorOnResize);
+window.addEventListener("resize", navbarColorOnResize);
 
-  function navbarColorOnResize() {
-    if (window.innerWidth > 1200) {
-      if (referenceButtons.classList.contains('active') && referenceButtons.getAttribute('data-class') === 'bg-transparent') {
-        sidenav.classList.remove('bg-white');
-      } else {
-        sidenav.classList.add('bg-white');
-      }
+function navbarColorOnResize() {
+  if (window.innerWidth > 1200) {
+    if (referenceButtons.classList.contains('active') && referenceButtons.getAttribute('data-class') === 'bg-transparent') {
+      sidenav.classList.remove('bg-white');
     } else {
       sidenav.classList.add('bg-white');
-      sidenav.classList.remove('bg-transparent');
     }
+  } else {
+    sidenav.classList.add('bg-white');
+    sidenav.classList.remove('bg-transparent');
   }
 }
+
 // Deactivate sidenav type buttons on resize and small screens
 window.addEventListener("resize", sidenavTypeOnResize);
 window.addEventListener("load", sidenavTypeOnResize);
